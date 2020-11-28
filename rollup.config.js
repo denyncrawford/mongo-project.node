35264536b10defe47bfce6d4b5693776f05b39e5
denyncrawford <kiquerangelb@gmail.com>
2020-11-28 13:46:35 -0300

    rebuilding for deno

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import commonjs from 'rollup-plugin-commonjs';
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'es'
+  }, 
+  plugins: [
+    commonjs({
+      exclude: "node_modules/**"
+    })
+  ]
+};
